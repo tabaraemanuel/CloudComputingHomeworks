ac757dd2b0460d63cd569adfeb72c34c6336e1e8
tabaraemi <emanuel.tabara@info.uaic.ro>
2021-02-25 20:19:12 +0200

    Tema1 initial commit

---
+++ Tema1/clientmain.py
@@ -0,0 +1,58 @@
+import socket
+import tkinter as tk
+import time
+import datetime
+import sys
+from time import sleep
+from functools import partial
+import os
+from PIL import ImageTk, Image
+HOST = '127.0.0.1'  # The server's hostname or IP address
+PORT = 8000  # The port used by the server
+DIRECT_PORT = 8000
+
+
+def startClient(entryField, root_local):
+    mesaj = entryField.get()
+    mesaj = bytearray(mesaj, "utf-8")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+        s.sendall(mesaj)
+        image_bytes = s.recv(1024 * 1024 * 10)
+        if sys.getsizeof(image_bytes) < 1024:
+            display_message = image_bytes.decode("utf-8")
+            rez_label = tk.Label(root_local, text=display_message)
+            rez_label.pack()
+            s.close()
+            return
+        unique = "photoclient" + datetime.datetime.now().strftime("%m%d%Y%H%M%S") + ".jpg"
+        fd = open(unique, "wb")
+        fd.write(image_bytes)
+        fd.close()
+        if not os.path.isfile(unique):
+            display_message = "Salvarea a esuat!"
+            rez_label = tk.Label(root_local, text=display_message)
+            rez_label.pack()
+            s.close()
+            return
+        display_message = "Download succes ! Cauta in dosarul local poza " + unique
+        rez_label = tk.Label(root_local, text=display_message)
+        img = ImageTk.PhotoImage(Image.open(unique))
+        panel = tk.Label(root_local, image=img)
+        panel.pack()
+        rez_label.pack()
+        s.close()
+
+
+if __name__ == '__main__':
+    root = tk.Tk()
+    welcomeLabel = tk.Label(root, text="Welcome !")
+    howtouseLabel = tk.Label(root, text="Input a Flickr userid and we will download for you the first image, "
+                                        "at a randomized size and compressed!")
+    entryField = tk.Entry(root, width=50)
+    sendButton = tk.Button(root, text="Send", command=partial(startClient, entryField, root))
+    welcomeLabel.pack()
+    howtouseLabel.pack()
+    entryField.pack()
+    sendButton.pack()
+    root.mainloop()
---
+++ Tema1/main.py
@@ -0,0 +1,168 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+import socket
+from _thread import start_new_thread
+import os
+from time import sleep
+import flickrapi
+from rdoclient import RandomOrgClient
+import tinify
+import datetime
+import urllib.request
+import urllib.response
+import os.path
+import io
+from PIL import Image
+
+# try to import C parser then fallback in pure python parser.
+try:
+    from http_parser.parser import HttpParser
+except ImportError:
+    from http_parser.pyparser import HttpParser
+
+HOST = '127.0.0.1'
+PORT = 8000
+
+
+def log(to_log):
+    to_log = to_log + '\n'
+    with open("logging.txt", "a") as f:
+        f.write(to_log)
+
+
+def monitor_log(to_log):
+    to_log = to_log + '\n'
+    with open("monitor.txt", "a") as f:
+        f.write(to_log)
+
+
+def monitor_and_abuse():
+    counter = 0
+    filename = "config.txt"
+    with open(filename) as f:
+        content = f.read().splitlines()
+    api_key = content[0]
+    api_secret = content[1]
+    flickr = flickrapi.FlickrAPI(api_key, api_secret, format='etree')
+    while True:
+        status = urllib.request.urlopen("https://www.random.org/quota/?format=plain").code
+        if 300 > status >= 200:
+            to_log = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " :Random.org is up and running"
+
+        else:
+            to_log = datetime.datetime.now().strftime(
+                "%m/%d/%Y, %H:%M:%S") + " :Random.org has problems, status code:" + str(status)
+        monitor_log(to_log)
+        status = urllib.request.urlopen("https://tinypng.com").code
+        if 300 > status >= 200:
+            to_log = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " :tinypng.com is up and running"
+
+        else:
+            to_log = datetime.datetime.now().strftime(
+                "%m/%d/%Y, %H:%M:%S") + " :tinypng.com has problems, status code:" + str(status)
+        monitor_log(to_log)
+        status = urllib.request.urlopen("https://www.flickr.com").code
+        if 300 > status >= 200:
+            to_log = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " :flickr.com is up and running"
+
+        else:
+            to_log = datetime.datetime.now().strftime(
+                "%m/%d/%Y, %H:%M:%S") + " :flickr.com has problems, status code:" + str(status)
+        monitor_log(to_log)
+        sleep(10)
+        counter -= -1
+        if counter > 10:
+            exit()
+
+
+def handle_client(conn, request):
+    filename = "config.txt"
+    with open(filename) as f:
+        content = f.read().splitlines()
+    api_key = content[0]
+    api_secret = content[1]
+    random_api_key = content[2]
+    tinify_api_key = content[3]
+    flickr = flickrapi.FlickrAPI(api_key, api_secret, format='etree')
+    user_id = request
+    to_log = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " :Sent flickr.FlickAPI request"
+    log(to_log)
+    try:
+        photos = flickr.photos.search(api_key=api_key, user_id=user_id, per_page='20')
+    except:
+        msj_bytes = b'Sorry, your id returned no photos!'
+        conn.sendall(msj_bytes)
+        conn.close()
+        exit(1)
+    to_log = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " :Sent flickr.photos.search request"
+    log(to_log)
+    random_gen = RandomOrgClient(random_api_key)
+    to_log = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " :Sent RandomOrgClient request"
+    log(to_log)
+    random_id = random_gen.generate_integers(1, 1, 8)[0]
+    to_log = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " :Sent random_gen.generate_integers request"
+    log(to_log)
+    to_log = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + ":Response from random_gen.generate_integers " \
+                                                                      "request was number " + str(random_id)
+    log(to_log)
+    sizes = ['s', 'q', 't', 'm', 'n', 'w', 'z', 'c', 'b']
+    size = sizes[random_id]
+    random_id = int(random_id)
+    enough = photo_id = photos.find('photos').attrib['total']
+    to_log = datetime.datetime.now().strftime(
+        "%m/%d/%Y, %H:%M:%S") + " :Response from flickr.photos.search request contains " + str(enough) + " photos"
+    log(to_log)
+    photo_id = photos.find('photos').findall('photo')[0].attrib['id']
+    secret_photo = photos.find('photos').findall('photo')[0].attrib['secret']
+    server_photo = photos.find('photos').findall('photo')[0].attrib['server']
+    link = "https://live.staticflickr.com/" + server_photo + "/" + photo_id + "_" + secret_photo + "_" + size + ".jpg"
+    print(link)
+    unique = "photo" + datetime.datetime.now().strftime("%m%d%Y%H%M%S") + ".jpg"
+    tinify.key = tinify_api_key
+    try:
+        source = tinify.from_url(link)
+        to_log = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + " :Sent compression request"
+        log(to_log)
+        source.to_file(unique)
+    except:
+        msj_bytes = b'Sorry, we ran out of compressions!'
+        conn.sendall(msj_bytes)
+        conn.close()
+        exit(1)
+    to_log = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S") + ":Response from compression request was " \
+                                                                      "succesful "
+    log(to_log)
+    imageFileObj = open(unique, "rb")
+    imageBinaryBytes = imageFileObj.read()
+    conn.sendall(imageBinaryBytes)
+    conn.close()
+    exit(1)
+
+
+def main_funcion():
+    start_new_thread(monitor_and_abuse, ())
+    while True:
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((HOST, PORT))
+            s.listen(10)
+            conn, addr = s.accept()
+            print('Connected by', addr)
+            data = conn.recv(1024)
+            request = data.decode("utf-8")
+            if request[:3] == "GET":
+                request = data.decode("utf-8")
+                request = request[5:]
+                a = ""
+                for j in request:
+                    if not j == ' ':
+                        a += j
+                    else:
+                        break
+            start_new_thread(handle_client, (conn, request,))
+
+
+if __name__ == '__main__':
+    main_funcion()
